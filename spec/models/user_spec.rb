30d46e627af70a3df88c3e71051b47ba06364349
ecruzgarrido <enrique.cruz.garrido@gmail.com>
2015-08-29 21:07:51 +0100

    Refactor user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,20 +4,14 @@ RSpec.describe User, type: :model do
 
   describe '#generate_authentication_token' do
 
-    let(:user) { User.make!(
-        name: 'User',
-        email: 'mail@example.com',
-        password: '12345678'
-    ) }
+    let(:user) { User.make! }
 
-    let(:authentication_token) { user.generate_authentication_token }
-
-    before(:each) do
-      authentication_token
+    before do
+      user.generate_authentication_token
     end
 
     it 'should win half the battle' do
-      expect(user.authentication_token).to eq(authentication_token)
+      expect(user.authentication_token).not_to be_blank
     end
 
   end
