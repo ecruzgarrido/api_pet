78a1cb90e89568714370c2f8460c065804074853
ecruzgarrido <enrique.cruz.garrido@gmail.com>
2015-08-30 19:35:22 +0100

    Remove dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,6 +1,7 @@
 ActiveAdmin.register_page "Dashboard" do
 
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
+  # menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
+  menu false
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
     div class: "blank_slate_container", id: "dashboard_default_message" do
@@ -9,6 +10,6 @@ ActiveAdmin.register_page "Dashboard" do
         small I18n.t("active_admin.dashboard_welcome.call_to_action")
       end
     end
-    
+
   end # content
 end
